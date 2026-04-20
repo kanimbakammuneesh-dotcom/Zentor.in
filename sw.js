0ea9fb33693e04b94d474ac72b506fcbe337fc51
kanimbakammuneesh <kanimbakammuneesh@gmail.com>
2026-04-20 18:55:50 +0530

    feat: add Monetag ad script tag to head

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10902015
+    "domain": "3nbf4.com",
+    "zoneId": 10902032
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
